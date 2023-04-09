a884e65f8648652b7d59560e310302c13ac4ab05
dzy7e <228218809@qq.com>
2023-04-09 19:24:32 +0800

    ml_caformer

--- demo_ca.py
+++ demo_ca.py
@@ -98,20 +98,40 @@ class Demo:
         img = self.trans(img)
         return img
 
-    def forward(self, img: torch.Tensor):
-        return torch.sigmoid(self.model(img)).cpu().view(-1)
+    def infer_one(self, img):
+        if self.args.fp16:
+            img = img.half()
+        img = img.unsqueeze(0)
+        output = torch.sigmoid(self.model(img)).cpu().view(-1)
+        pred = torch.where(output > self.args.thr)[0].numpy()
+
+        cls_list = [(self.class_map[str(i)], output[i]) for i in pred]
+        return cls_list
 
     @torch.no_grad()
     def infer(self, path):
-        img = self.load_data(path).to(device)
-        if self.args.fp16:
-            img=img.half()
-        img = img.unsqueeze(0)
-        output = self.forward(img)
-        pred = torch.where(output > self.args.thr)[0].numpy()
+        if os.path.isfile(path):
+            img = self.load_data(path).to(device)
+            cls_list = self.infer_one(img)
+            return cls_list
+        else:
+            tag_dict = {}
+            img_list = [os.path.join(path, x) for x in os.listdir(path) if x[x.rfind('.'):].lower() in IMAGE_EXTENSIONS]
+            for item in tqdm(img_list):
+                img = self.load_data(item).to(device)
+                cls_list = self.infer_one(img)
+                cls_list.sort(reverse=True, key=lambda x: x[1])
+                if self.args.out_type == 'txt':
+                    with open(item[:item.rfind('.')] + '.txt', 'w', encoding='utf8') as f:
+                        f.write(', '.join([name.replace('_', ' ') for name, prob in cls_list]))
+                elif self.args.out_type == 'json':
+                    tag_dict[os.path.basename(item)] = ', '.join([name.replace('_', ' ') for name, prob in cls_list])
 
-        cls_list=[(self.class_map[str(i)], output[i]) for i in pred]
-        return cls_list
+            if self.args.out_type == 'json':
+                with open(os.path.join(path, 'image_captions.json'), 'w', encoding='utf8') as f:
+                    f.write(json.dumps(tag_dict))
+
+            return None
 
 #python demo_ca.py --data imgs/t1.jpg --model_name caformer_m36 --ckpt ckpt/caformer_m36-2-20000.ckpt --thr 0.7 --image_size 448
 if __name__ == '__main__':
